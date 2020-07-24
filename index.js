3a61a45b82c0541e4922055e1baf566484f6fa9e
Kartik <kartikmandhan14@gmail.com>
2020-07-24 22:14:04 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+AOS.init();
+
+window.onscroll = function() {
+  let navbar = $(".navbar");
+  if (document.documentElement.scrollTop >= 450) {
+    navbar.addClass("bg-new");
+  } else {
+    navbar.removeClass("bg-new");
+  }
+};
+
+var btnContainer = $(".btn-container")[0];
+      var btns = btnContainer.getElementsByClassName("btn");
+      var tabcontent, tablinks;
+      tabcontent = document.getElementsByClassName("tabcontent");
+      
+      for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener("click", function() {
+          const current = $(".btn-container .active")[0];
+          current.className = current.className.replace(" active", "");
+          this.className += " active";
+          // console.log(this.value);
+          const buttonMadeActive = this.value;
+          console.log(buttonMadeActive);
+          for (var i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+          }
+          document.getElementById(buttonMadeActive).style.display="block";
+        });
+        }
